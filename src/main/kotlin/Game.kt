63f67949e70f0ad339215e8deebac6e9311d00ad
qqqsss007 <qqqsss007@mail.ru>
2021-03-23 21:25:23 +0700

    Govno

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val x: int = 7
+    val y: int = 8
+    printSum(x, y)
+}
+
+fun printSum(int1: int, int2: int){
+    val result = int + int2
+    println(result)
+}
+
+//page 95
